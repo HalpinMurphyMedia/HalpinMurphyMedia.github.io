2cfe4a5780ad914cf4905fdd56c502d488973364
HalpinMurphyMedia <footiestats@yahoo.ie>
2016-04-11 10:08:28 +0100

    Removed unused script
    Removed all unused script from the custom highcharts and clearly named
    the remaining  #ids

--- scripts/game_script.js
+++ scripts/game_script.js
@@ -377,11 +377,13 @@ function myFunction(xml)
 function loadCharts()
   {
 //----------------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- First Chart --
+  // -- First Chart --													GOALS
     $('#container').highcharts({
         chart: {
             type: 'bar'
         },
+// The next line allows for custom colours to be used but because both parts of chart are part of one series. So they only take the one colour attribute. If we add another series it would create another bar/pie chart in the same style.
+		colors: ['#0000FF', '#006600',],
         title: {
             text: 'Goals'
         },
@@ -423,13 +425,13 @@ function loadCharts()
             enabled: false
         },
         series: [{
-            name: '',
+            name: 'Goals',
             data: [homeGoals, awayGoals]
         }]
     });
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- Second Chart
+  // -- Second Chart											ATTENDANCE
   $('#container2').highcharts({
         chart: {
             plotBackgroundColor: null,
@@ -455,7 +457,7 @@ function loadCharts()
             }
         },
         series: [{
-//            name: 'Attendance',
+            name: 'Attendance',
             colorByPoint: true,
             data: [{
                 name: "Filled Seats",
@@ -470,101 +472,9 @@ function loadCharts()
   // -- End Container2 --
     });
   
-// -------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- 3rd Chart --
-    $('#container3').highcharts({
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Shots off Target'
-        },
-        xAxis: {
-            categories: ['Team 1', 'Team 2'],
-            title: {
-                text: null
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Shots off Target',
-                align: 'high'
-            },
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    enabled: true
-                }
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            x: -40,
-            y: 80,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-            shadow: true
-        },
-        credits: {
-            enabled: false
-        },
-        series: [{
-            name: '',
-            data: [6, 4]
-        }]
-    });
+  //----------------------------------------------------------------------------------------------------------------------------------------------------------
   
-// -----------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- Fourth Chart
-  $('#container4').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: 'Saves'
-        },
-
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'Saves',
-            colorByPoint: true,
-            data: [{
-                name: 'Team 1',
-                y: 3
-            }, {
-                name: 'Team 2',
-                y: 6,
-                sliced: true,
-                selected: true
-            }]
-        }]
-  // -- End Container4 --
-    });
-	//----------------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- 5th Chart --
+  // -- 3th Chart --											YELLOW CARDS
     $('#container5').highcharts({
         chart: {
             type: 'bar'
@@ -610,13 +520,13 @@ function loadCharts()
             enabled: false
         },
         series: [{
-            name: '',
+            name: 'Yellow Cards',
             data: [homeYellowAmount, awayYellowAmount]
         }]
     });
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- 6th Chart
+  // -- 4th Chart															RED CARDS
   $('#container6').highcharts({
         chart: {
             plotBackgroundColor: null,
@@ -642,7 +552,7 @@ function loadCharts()
             }
         },
         series: [{
-            name: 'Reds',
+            name: 'Red Cards',
             colorByPoint: true,
             data: [{
                 name: teamOne,
@@ -657,290 +567,9 @@ function loadCharts()
   // -- End Container6 --
     });
   
-// -------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- 7th Chart --
-    $('#container7').highcharts({
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Headers'
-        },
-        xAxis: {
-            categories: ['Team 1', 'Team 2'],
-            title: {
-                text: null
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Headers',
-                align: 'high'
-            },
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    enabled: true
-                }
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            x: -40,
-            y: 80,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-            shadow: true
-        },
-        credits: {
-            enabled: false
-        },
-        series: [{
-            name: '',
-            data: [11, 7]
-        }]
-    });
-  
-// -----------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- 8th Chart
-  $('#container8').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: 'Interceptions'
-        },
-
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'Interceptions',
-            colorByPoint: true,
-            data: [{
-                name: 'Team 1',
-                y: 5
-            }, {
-                name: 'Team 2',
-                y: 3,
-                sliced: true,
-                selected: true
-            }]
-        }]
-  // -- End Container8 --
-    });
-	
-	//----------------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- 9th Chart --
-    $('#container9').highcharts({
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Tackles'
-        },
-        xAxis: {
-            categories: ['Team 1', 'Team 2'],
-            title: {
-                text: null
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Tackles',
-                align: 'high'
-            },
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    enabled: true
-                }
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            x: -40,
-            y: 80,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-            shadow: true
-        },
-        credits: {
-            enabled: false
-        },
-        series: [{
-            name: '',
-            data: [17, 9]
-        }]
-    });
-
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- 10th Chart
-  $('#container10').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: 'Fouls'
-        },
-
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'Fouls',
-            colorByPoint: true,
-            data: [{
-                name: 'Team 1',
-                y: 11
-            }, {
-                name: 'Team 2',
-                y: 9,
-                sliced: true,
-                selected: true
-            }]
-        }]
-  // -- End Container10 --
-    });
-  
-// -------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- 11th Chart --
-    $('#container11').highcharts({
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Bookings'
-        },
-        xAxis: {
-            categories: ['Team 1', 'Team 2'],
-            title: {
-                text: null
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Bookings',
-                align: 'high'
-            },
-            labels: {
-                overflow: 'justify'
-            }
-        },
-        plotOptions: {
-            bar: {
-                dataLabels: {
-                    enabled: true
-                }
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'top',
-            x: -40,
-            y: 80,
-            floating: true,
-            borderWidth: 1,
-            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-            shadow: true
-        },
-        credits: {
-            enabled: false
-        },
-        series: [{
-            name: 'yellow cards',
-            data: [1, 4]
-        }]
-    });
-  
-// -----------------------------------------------------------------------------------------------------------------------------------------------------
-  // -- 12th Chart
-  $('#container12').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: 'Offsides'
-        },
-
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-        series: [{
-            name: 'Offsides',
-            colorByPoint: true,
-            data: [{
-                name: 'Team 1',
-                y: 5
-            }, {
-                name: 'Team 2',
-                y: 3,
-                sliced: true,
-                selected: true
-            }]
-        }]
-  // -- End Container12 --
-    });
 
 // _________________________________________________________________________________________________________________________________________________
-// Page Reloading
+// Page Reloading														PAGE RELOADING
 
 // Calls a function to reload the page every 30 seconds based on a timer and clears all existing data on the page. This reloads the entire page
 // This will only be enabled on pages that contain data that may change.
